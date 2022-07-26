b8a219ddf3b2e0515278481039024997c2c571c6
jordanLindo <jordanlindo@live.com>
2022-07-26 04:10:32 -0500

    Fixed errors

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import './App.css';
 import React from 'react';
+import Header from './Header.js';
+import Main from './Main';
+import Footer from './Footer';
 
 class App extends React.Component {
   render(){
     return(
       <>
-        <Header />
-        <Main />
-        <Footer />
+        <Header/>
+        <Main/>
+        <Footer/>
       </>
     );
   }
