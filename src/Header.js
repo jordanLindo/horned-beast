d08cc7253e62e2794a2a2eecd9fd0c589e8aeedc
jordanLindo <jordanlindo@live.com>
2022-07-26 15:55:21 -0500

    Added Lab-02 work

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,30 @@
 import React from 'react';
 
 class Header extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            isActive: false
+        };
+    };
+
+    setIsActive = () =>{
+        if(this.state.isActive){
+            this.setState({
+                isActive: false
+            })
+        } else {
+            this.setState({
+                isActive: true
+            })
+        }
+    }
+
     render(){
+        const colorClass = this.state.isActive ? 'blue' : 'red';
         return(
-            <header>
-                <h1>Horned Beasts</h1>
+            <header className={colorClass} onClick={this.setIsActive}>
+                <h1>{this.state.isActive ? 'Pointy Friends' : 'Horned Beasts'}</h1>
             </header>
         );
     }
--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -1,16 +1,32 @@
 import React from "react";
+import Col from 'react-bootstrap/Col';
 
-class HornedBeast extends React.Component{
+class HornedBeast extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            likes: 0
+        }
+    }
+
+    handleLikes = () => {
+        this.setState({
+            likes: this.state.likes + 1
+        });
+    };
+
     render() {
         return (
-            <>
+            <Col>
                 <h2>{this.props.title}</h2>
-                <img src={this.props.imageUrl} alt={this.props.title} title={this.props.title} />
+                <img src={this.props.imageUrl} alt={this.props.title} title={this.props.title} onClick={this.handleLikes} />
+                <p>	&#9825;{this.state.likes}</p>
                 <p>{this.props.description}</p>
-
-            </>
+            </Col>
         );
     }
 }
+
+
 
 export default HornedBeast;
